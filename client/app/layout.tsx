b4c902c04e392b8436aeee191dc59c10de85e28c
developer-samara-web <developer.samara.web@gmail.com>
2026-08-17 23:59:37 +0400

    feat: add lint, priettier

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,15 +1,15 @@
-import type { Metadata } from "next";
-import "./globals.css";
+import type { Metadata } from 'next'
+import './globals.css'
 
 export const metadata: Metadata = {
-	title: "Example project",
-	description: "Example project description",
-};
+	title: 'Example project',
+	description: 'Example project description',
+}
 
-export default function RootLayout({ children }: LayoutProps<"/">) {
+export default function RootLayout({ children }: LayoutProps<'/'>) {
 	return (
 		<html lang="en">
 			<body>{children}</body>
 		</html>
-	);
+	)
 }
